50739c8d76ae471eb31598ecef087f1365ca48af
Amy <wilford.amy@gmail.com>
2022-10-18 16:02:01 -0400

    set up database schema and seeded starter information

---
+++ db/seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO department(name)
+VALUES  ('Research'), 
+        ('Slaying'), 
+        ('Maintenance'), 
+        ('Consultants');
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ('Slayer', 50000, 2),
+        ('Watcher', 60000, 1), 
+        ('Researcher', 40000, 1),
+        ('Maintenance', 40000, 3), 
+        ('Deamon Consultant', 50000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ('Buffy', 'Summers', 1, 2), 
+        ('Rupert', 'Giles', 2, null),
+        ('Willow', 'Rosenberg', 3, 2),
+        ('Zander', 'Harris', 4, null), 
+        ('Anya', 'Jenkins', 5, 1);
