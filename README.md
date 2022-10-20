192b34b4f6e8a558e51f9989c1f25668dbc3b44e
Amy <wilford.amy@gmail.com>
2022-10-19 21:00:44 -0400

    README.md file written

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# Employee-Tracker
+# Scooby Gang Team Tracker:
+
+## Table of Contents:
+
+- [Description](#description)
+- [Technologies-Used](#technologies-used)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Demo](#demo)
+- [License](#license)
+
+## Description:
+
+The best way to keep track of Sunnydale's Scooby Gang and ensure things are taken care of behind the scenes. After all, slaying vampires is hard work - the office admin doesn't have to be. 
+
+This application runs as an interative CLI  (using nodes Inquirier) to view - update - add - and delete information stored in the connected SQL database. Any details changed/updated through the command line will be automatically updated in the SQL database (employee_db). 
+
+## Technologies Used:
+
+- SQL 
+- Node.js
+- NPM Inquirer Package, v. 8.2.4
+- NPM Mysql2
+- NPM figlet
+- NPM console.table
+
+## Installation:
+
+This application requires npm package dependencies.
+For installation, in your terminal command line, run:
+
+```
+npm install
+```
+
+## Usage:
+
+To use: Open an integrated terminal linked to the index.js file. Run npm start in the terminal command line to launch the application.
+
+Once launched, you will see a welcome message, and a list of options to either view - add to - update - or delete team details. 
+
+## Demo:
+
+- [View a video walk through of this application in use]()
+
+## License:
+
+N/A
--- db/query.sql
+++ db/query.sql
@@ -1,5 +1,4 @@
 -- VIEW ALL EMPLOYEES:
--- THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 SELECT e.id AS 'Employee ID', 
     e.first_name AS 'First Name', 
     e.last_name AS 'Last Name', 
@@ -26,42 +25,21 @@ SELECT CONCAT(m.first_name, ' ', m.last_name) AS 'Manager',
     WHERE e.manager_id IS NOT NULL
     ORDER BY m.id;
     
-
--- ADD EMPLOYEE:
--- THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-INSERT INTO employee (first_name, last_name, role_id, manager_id) 
-VALUES (?,?,?,?)
-
--- UPDATE EMPLOYEE ROLE:
--- THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
-
-
 -- VIEW ALL ROLES:
--- THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
 SELECT role.title AS 'Role', role.id AS 'Role ID', department.name AS 'Department', role.salary AS 'Salary'
 FROM role
 JOIN department
 ON role.department_id = department.id
 ORDER BY role.id;
 
--- ADD ROLES:
--- THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-INSERT INTO role (title, salary, department_id) 
-VALUES(?,?,?)
-
 -- VIEW ALL DEPARTMENTS
--- THEN I am presented with a formatted table showing department names and department ids
  SELECT id AS 'Department ID', name AS 'Department'
  FROM department
  ORDER BY department.id;
 
 -- ADD DEPARTMENT
--- THEN I am prompted to enter the name of the department and that department is added to the database
 INSERT INTO department(name)
 VALUES  ('department');
-
-
-
 
 -- VIEW EMPLOYEE DEPARTMENTS
 SELECT  department.name AS 'Department', 
@@ -70,7 +48,6 @@ SELECT  department.name AS 'Department',
     JOIN role ON employee.role_id = role.id
     JOIN department ON role.department_id = department.id
     ORDER BY department.name;
-
 
 -- VIEW DEPARTMENT BUDGETS
 SELECT department_id AS 'Department ID',
